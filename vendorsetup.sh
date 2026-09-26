1dbc3c9ea01c7dfee0fcfe038c226988c01ddb4e
balajinatarajan21 <46074342+balajinatarajan21@users.noreply.github.com>
2026-09-26 20:37:16 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,16 +1,16 @@
 echo "Cloning required repositories for OnePlus SM8250..."
 
 # Common Vendor
-git clone https://github.com/spidey-t/vendor_oneplus_sm8250-common.git -b lineage-24-test vendor/oneplus/sm8250-common
+git clone https://github.com/spidey-t/vendor_oneplus_sm8250-common.git -b vos vendor/oneplus/sm8250-common
 
 # Common Device
-git clone https://github.com/spidey-t/device_oneplus_sm8250-common.git -b lineage-24-test device/oneplus/sm8250-common 
+git clone https://github.com/spidey-t/device_oneplus_sm8250-common.git -b vos device/oneplus/sm8250-common 
 
 # Lemonades Vendor
 git clone https://github.com/spidey-t/vendor_oneplus_lemonades.git -b lineage-23.2 vendor/oneplus/lemonades
 
 # Hardware Oplus
-git clone https://github.com/spidey-t/hardware_oplus.git hardware/oplus
+git clone https://github.com/spidey-t/hardware_oplus.git -b vos hardware/oplus
 
 # Kernel
 git clone https://github.com/spidey-t/kernel_oneplus_sm8250.git -b lineage-24-test kernel/oneplus/sm8250
